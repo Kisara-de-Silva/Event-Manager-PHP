eb8fa8f70a0dbe8b662d703c58aeca05cb34c2ec
Kisara de Silva <manmithdesilva@gmail.com>
2025-05-15 02:19:53 +0530

    Update navigation.blade.php
    Adding ADMIN updates.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,19 +13,22 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     @auth
-                        @if (auth()->user()->role === 'admin')
-                            <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
-                                {{ __('Admin Dashboard') }}
-                            </x-nav-link>
-                        @else
-                            <x-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')">
-                                {{ __('User Dashboard') }}
-                            </x-nav-link>
-                        @endif
-
-                        <x-nav-link :href="route('events.index')" :active="request()->routeIs('events.*')">
-                            {{ __('My Events') }}
+                        <x-nav-link :href="route(auth()->user()->role === 'admin' ? 'admin.dashboard' : 'user.dashboard')" 
+                                   :active="request()->routeIs('*.dashboard')">
+                            {{ __('Dashboard') }}
                         </x-nav-link>
+
+                        <x-nav-link :href="route('events.index')" 
+                                   :active="request()->routeIs('events.*')">
+                            {{ auth()->user()->role === 'admin' ? __('All Events') : __('My Events') }}
+                        </x-nav-link>
+                        
+                        @if(auth()->user()->role === 'admin')
+                        <x-nav-link :href="route('users.index')" 
+                                   :active="request()->routeIs('users.*')">
+                            {{ __('User Management') }}
+                        </x-nav-link>
+                        @endif
                     @endauth
                 </div>
             </div>
@@ -36,7 +39,6 @@
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
-
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -80,19 +82,22 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             @auth
-                @if (auth()->user()->role === 'admin')
-                    <x-responsive-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
-                        {{ __('Admin Dashboard') }}
-                    </x-responsive-nav-link>
-                @else
-                    <x-responsive-nav-link :href="route('user.dashboard')" :active="request()->routeIs('user.dashboard')">
-                        {{ __('User Dashboard') }}
-                    </x-responsive-nav-link>
-                @endif
-
-                <x-responsive-nav-link :href="route('events.index')" :active="request()->routeIs('events.*')">
-                    {{ __('My Events') }}
+                <x-responsive-nav-link :href="route(auth()->user()->role === 'admin' ? 'admin.dashboard' : 'user.dashboard')" 
+                                     :active="request()->routeIs('*.dashboard')">
+                    {{ __('Dashboard') }}
                 </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('events.index')" 
+                                     :active="request()->routeIs('events.*')">
+                    {{ auth()->user()->role === 'admin' ? __('All Events') : __('My Events') }}
+                </x-responsive-nav-link>
+                
+                @if(auth()->user()->role === 'admin')
+                <x-responsive-nav-link :href="route('users.index')" 
+                                     :active="request()->routeIs('users.*')">
+                    {{ __('User Management') }}
+                </x-responsive-nav-link>
+                @endif
             @endauth
         </div>
 
